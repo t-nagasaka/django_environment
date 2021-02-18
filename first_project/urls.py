ab5ed801122d11f9ceebbd2fce83a7e25fbfd4e0
Honami Tomita <mikmik0115@gmail.com>
2021-02-18 17:05:32 +0000

    edit about

--- first_project/urls.py
+++ first_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('signin.urls')),
+    path('', include('about.urls')),
+    path('signin/', include('signin.urls')),
     path('signup/', include('signup.urls')),
 ]
